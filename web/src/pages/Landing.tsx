4f5463162916436964ffd2e1aa54189d3d268e2e
agftech <agiraof@gmail.com>
2020-10-12 19:49:17 -0300

    :package: feat: add page navigation

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowRight } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
 
@@ -21,9 +22,9 @@ function Landing() {
         <span>Brasil</span>
       </div>
 
-      <a href='' className='enter-app'>
+      <Link to='/app' className='enter-app'>
         <FiArrowRight size={26} color='rgba(8, 0, 0, 0.6)' />
-      </a>
+      </Link>
     </div>
   </div>
   );
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function OrphanagesMap() {
-  return ( 
-    <h1>Orphanages Map</h1>
-  );
+	return (
+		<div>
+			<h1>Orphanages Map</h1>
+			<Link to='' className='enter-app'>
+				<FiArrowLeft size={26} color='rgba(8, 0, 0, 0.6)' />
+			</Link>
+		</div>
+	);
 }
 
 export default OrphanagesMap;
