937a55aed8f7a23becf0c02f0984faaa724f75db
agftech <agiraof@gmail.com>
2020-10-12 18:53:17 -0300

    :dizzy: style: add style to landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './styles/global.css';
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Happy App</h1>
-    </div>
-  );
+	return (
+		<div id='page-landing'>
+			<div className='content-wrapper'>
+				<img src={logoImg} alt='Logo da plataforma Happy' />
+
+				<main>
+					<h1>Leve felicidade para o mundo</h1>
+					<p>Visite orfanatos e mude o dia de muitas crianças.</p>
+				</main>
+
+				<div className='location'>
+					<strong>Brasil</strong>
+					<span>Brasil</span>
+				</div>
+
+				<a href="" className='enter-app'>
+          :)
+        </a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
