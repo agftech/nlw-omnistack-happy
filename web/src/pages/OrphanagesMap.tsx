e2b9890451f5b87c20d3062dd341d73d23b700d1
agftech <agiraof@gmail.com>
2020-10-12 19:58:27 -0300

    :package: feat: update page OrphanagesMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { FiArrowLeft } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
 import { Link } from "react-router-dom";
+
+import mapMakerImg from '../images/map-marker-happy.svg'
 
 function OrphanagesMap() {
 	return (
-		<div>
-			<h1>Orphanages Map</h1>
-			<Link to='' className='enter-app'>
-				<FiArrowLeft size={26} color='rgba(8, 0, 0, 0.6)' />
-			</Link>
-		</div>
+		<div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Logo da plataforma Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Brasil</strong>
+          <span>Brasil</span>
+        </footer>
+      </aside>
+
+			<div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+
+    </div>
 	);
 }
 
