58bf4d7af6b45c57ce44bc3e7cd9661f1224373d
agftech <agiraof@gmail.com>
2020-10-13 20:36:27 -0300

    :package: feat: add access permission

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const path = require("path");
+import cors from "cors";
 import "express-async-errors";
 import errorHandler from "./errors/handler";
 
@@ -9,6 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
