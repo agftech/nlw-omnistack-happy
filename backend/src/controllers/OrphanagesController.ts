cd0ce3cc7dd70ac707706feca7111134453fcf8e
agftech <agiraof@gmail.com>
2020-10-13 20:23:34 -0300

    :package: feat: add data validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import * as Yup from "yup";
 
 import Orphanage from "../models/Orphanage";
+
 import orphanageView from "../views/orphanages_view";
 
 export default {
@@ -40,7 +42,7 @@ export default {
 			return { path: image.filename };
 		});
 
-		const orphanage = orphanagesRepository.create({
+		const data = {
 			name,
 			latitude,
 			longitude,
@@ -49,7 +51,28 @@ export default {
 			opening_hours,
 			open_on_weekends,
 			images,
+		};
+
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			latitude: Yup.number().required(),
+			longitude: Yup.number().required(),
+			about: Yup.string().required().max(300),
+			instructions: Yup.string().required(),
+			opening_hours: Yup.string().required(),
+			open_on_weekends: Yup.boolean().required(),
+			images: Yup.array(
+				Yup.object().shape({
+					path: Yup.string().required(),
+				})
+			),
 		});
+
+		await schema.validate(data, {
+			abortEarly: false,
+		});
+
+		const orphanage = orphanagesRepository.create(data);
 
 		await orphanagesRepository.save(orphanage);
 
--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,19 @@
 import { ErrorRequestHandler } from "express";
+import { ValidationError } from "yup";
+
+interface ValidationErrors {
+	[key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, req, resp, next) => {
+	if (error instanceof ValidationError) {
+		let errors: ValidationErrors = {};
+		error.inner.forEach((err) => {
+			errors[err.path] = err.errors;
+		});
+		return resp.status(400).json({ message: "Validation fails", errors });
+	}
+
 	console.log(error);
 	return resp.status(500).json({ message: "Internal server error" });
 };
