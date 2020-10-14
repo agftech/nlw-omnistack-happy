de8766e2c00929b6e724c51b6cd1ca8d5ce46155
agftech <agiraof@gmail.com>
2020-10-14 15:16:33 -0300

    :bug: fix: resolve access permission error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
