e6707d6b1e32bb2899d62fde0397d0034bcda9d1
agftech <agiraof@gmail.com>
2020-10-15 09:22:18 -0300

    :package: feat: add API connection on Orphanage

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { FaWhatsapp } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+//import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import Sidebar from "../components/Sidebar";
 
@@ -10,7 +10,41 @@ import mapIcon from "../utils/mapIcon";
 
 import "../styles/pages/orphanage.css";
 
+import api from "../services/api";
+
+interface Orphanage {
+	name: string;
+  latitude: number;
+  longitude: number;
+	about: string;
+	instructions: string;
+	opening_hours: string;
+	open_on_weekends:boolean;
+	images: Array<{
+		id: number;
+		url: string;
+	}>;
+}
+
+interface OrphanageParams {
+	id: string;
+}
+
 export default function Orphanage() {
+
+	const params = useParams<OrphanageParams>();
+
+	const [orphanage, setOrphanage] = useState<Orphanage>();
+
+  useEffect(() => {
+		api.get(`orphanages/${params.id}`).then((response) => {
+			setOrphanage(response.data);
+		});
+	}, [params.id]);
+	
+	if(!orphanage){
+		return <p>Carregando...</p>
+	}
 	
 	return (
 		<div id='page-orphanage'>
@@ -20,59 +54,32 @@ export default function Orphanage() {
 			<main>
 				<div className='orphanage-details'>
 					<img
-						src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
+						src={orphanage.images[1].url}
 						alt='Lar das meninas'
 					/>
 
 					<div className='images'>
-						<button className='active' type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
-						<button type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
-						<button type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
-						<button type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
-						<button type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
-						<button type='button'>
-							<img
-								src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-								alt='Lar das meninas'
-							/>
-						</button>
+						{orphanage.images.map(image => {
+							return (
+								<button key={image.id} className='active' type='button'>
+								<img
+									src={image.url}
+									alt={orphanage.name}
+								/>
+							</button>
+							);
+						})}
 					</div>
 
 					<div className='orphanage-details-content'>
-						<h1>Lar das meninas</h1>
+						<h1>{orphanage.name}</h1>
 						<p>
-							Presta assistência a crianças de 06 a 15 anos que se encontre em
-							situação de risco e/ou vulnerabilidade social.
+							{orphanage.about}
 						</p>
 
 						<div className='map-container'>
 							<Map
-								center={[-27.2092052, -49.6401092]}
+								center={[orphanage.latitude, orphanage.longitude]}
 								zoom={16}
 								style={{ width: "100%", height: 280 }}
 								dragging={false}
@@ -87,7 +94,7 @@ export default function Orphanage() {
 								<Marker
 									interactive={false}
 									icon={mapIcon}
-									position={[-27.2092052, -49.6401092]}
+									position={[orphanage.latitude, orphanage.longitude]}
 								/>
 							</Map>
 
@@ -100,26 +107,36 @@ export default function Orphanage() {
 
 						<h2>Instruções para visita</h2>
 						<p>
-							Venha como se sentir mais à vontade e traga muito amor para dar.
+							{orphanage.instructions}
 						</p>
 
 						<div className='open-details'>
 							<div className='hour'>
 								<FiClock size={32} color='#15B6D6' />
 								Segunda à Sexta <br />
-								8h às 18h
+								{orphanage.opening_hours}
 							</div>
-							<div className='open-on-weekends'>
-								<FiInfo size={32} color='#39CC83' />
-								Atendemos <br />
-								fim de semana
-							</div>
+							{
+									orphanage.open_on_weekends ? (
+										<div className='open-on-weekends'>
+											<FiInfo size={32} color='#39CC83' />
+											Atendemos <br />
+											fim de semana
+										</div>
+									) : (
+										<div className='open-on-weekends dont-open'>
+											<FiInfo size={32} color='#FF6690' />
+											Não atendemos <br />
+											fim de semana
+										</div>
+									)
+							}
 						</div>
 
-						<button type='button' className='contact-button'>
+{/* 						<button type='button' className='contact-button'>
 							<FaWhatsapp size={20} color='#FFF' />
 							Entrar em contato
-						</button>
+						</button> */}
 					</div>
 				</div>
 			</main>
