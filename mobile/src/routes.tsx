ba76edbc2496597d212a3808c45df373050d9946
agftech <agiraof@gmail.com>
2020-10-15 20:11:39 -0300

    :package: feat: add OrphanageDetails page

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,12 +5,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
       </Navigator>
     </NavigationContainer>
   );
