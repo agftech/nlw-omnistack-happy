ddad6cae4641b0ad64ed4f3681d437fe1b15b82f
agftech <agiraof@gmail.com>
2020-10-15 19:33:49 -0300

    :dizzy: style: add nunito fontFamily

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,10 +3,27 @@ import React from "react";
 import {StyleSheet, View, Dimensions, Text, TouchableOpacity} from "react-native";
 import MapView, {Callout, Marker, PROVIDER_GOOGLE} from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
 
 import mapMarker from "./src/images/map-marker-happy.png";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -70,6 +87,7 @@ const styles = StyleSheet.create({
   calloutText: {
     color: "#0089a5",
     fontSize: 14,
+    fontFamily: "Nunito_700Bold",
   },
   footer: {
     position: "absolute",
@@ -90,6 +108,7 @@ const styles = StyleSheet.create({
   },
 
   footerText: {
+    fontFamily: "Nunito_700Bold",
     color: "#8fa7b3",
   },
 
