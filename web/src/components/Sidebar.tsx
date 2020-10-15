450ace6f519c7fde2e99a43b4a40a7319164a141
agftech <agiraof@gmail.com>
2020-10-15 15:00:41 -0300

    :package: feat: add sidebar link to homepage

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import mapMarkerImg from '../images/map-marker-happy.svg';
 
@@ -10,7 +10,9 @@ export default function Sidebar() {
   const { goBack } = useHistory();
   return ( 
     <aside className="app-sidebar">
-      <img src={mapMarkerImg} alt="Happy" />
+      <Link to='/' >
+          <img src={mapMarkerImg} alt="Home" />
+      </Link>
 
       <footer>
         <button type="button" onClick={goBack}>
