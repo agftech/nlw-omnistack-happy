d5578b818c228761989e9f5255fa1cf3d7a2e6b0
agftech <agiraof@gmail.com>
2020-10-16 16:04:45 -0300

    :package: feat: add SelectMapPosition and OrphanageData pages

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -7,13 +7,19 @@ const { Navigator, Screen } = createStackNavigator();
 import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
 
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
+
 export default function Routes() {
-  return (
-    <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
-      </Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Navigator screenOptions={{ headerShown: false }}>
+				<Screen name='OrphanagesMap' component={OrphanagesMap} />
+				<Screen name='OrphanageDetails' component={OrphanageDetails} />
+
+				<Screen name='SelectMapPosition' component={SelectMapPosition} />
+				<Screen name='OrphanageData' component={OrphanageData} />
+			</Navigator>
+		</NavigationContainer>
+	);
 }
