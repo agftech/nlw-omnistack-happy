5344e3c083d8b9bf15aa1235c7a43032fdf7aad3
agftech <agiraof@gmail.com>
2020-10-17 18:16:26 -0300

    :recycle: refactor: update from useEffect() to useFocusEffect()

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Dimensions, Text } from "react-native";
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 
 import mapMarker from "../images/map-marker-happy.png";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
 
 import api from "../services/api";
@@ -19,11 +19,11 @@ interface Orphanage {
 export default function OrphanagesMap() {
 	const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
-	useEffect(() => {
+	useFocusEffect(() => {
 		api.get("orphanages").then((response) => {
 			setOrphanages(response.data);
 		});
-	}, []);
+	});
 	const navigation = useNavigation();
 
 	function handleNavigateToOrphanageDetails(id: number) {
