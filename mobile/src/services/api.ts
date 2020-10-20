6a1a794a68f343e40939267ce047313d70bce41d
agftech <agiraof@gmail.com>
2020-10-20 14:18:41 -0300

    :building_construction: build: set environment variables

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.EXPO_APP_BASE_URL;
+
 const api = axios.create({
-	baseURL: "http://192.168.1.3:3333",
+	//baseURL: "http://192.168.1.5:3333",
+	baseURL: `http://${baseURL}`,
 });
 
 export default api;
