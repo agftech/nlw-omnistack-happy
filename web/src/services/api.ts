0357c8f622ded206b5766126ffe6e0f4c4c903e7
agftech <agiraof@gmail.com>
2020-10-20 12:26:25 -0300

    :building_construction: build: set environment variables

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_BASE_URL;
+
 const api = axios.create({
-	baseURL: "http://localhost:3333",
+	baseURL: `http://${baseURL}`,
 });
 
 export default api;
