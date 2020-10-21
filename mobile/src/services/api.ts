b325347cdfde96fac6d4f9c1be1b7dc2f1bc3e52
agftech <agiraof@gmail.com>
2020-10-21 10:01:04 -0300

    :recycle: refactor: removes unnecessary comments

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const baseURL = process.env.EXPO_APP_BASE_URL;
 
 const api = axios.create({
-	//baseURL: "http://192.168.1.5:3333",
 	baseURL: `http://${baseURL}`,
 });
 
